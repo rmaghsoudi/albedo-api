e9c07aa76775b8ba5afde88701d43301ecff180b
Romy Maghsoudi <romy_mag@outlook.com>
2019-12-08 17:11:38 -0500

    removed CORS config

--- app.js
+++ app.js
@@ -41,19 +41,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(jwtCheck);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
-
 app.get('/authorized', function (req, res) {
     res.send('Secured Resource');
 });
