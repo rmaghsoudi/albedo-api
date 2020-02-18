1cc387318f9580251c730440f6272f9133e2d9e3
Romy Maghsoudi <romy.maghsoudi@macys.com>
2020-02-18 07:58:47 -0500

    addTest functionality

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -1,17 +1,23 @@
 import mongoose from 'mongoose';
 import { testSchema } from '../models/testModel';
+import { userSchema } from '../models/userModel';
 
 const Test = mongoose.model('Test', testSchema);
+const User = mongoose.model('User', userSchema);
 
-export const addNewTest = (req, res) => {
-  let newTest = new Test(req.body);
-
-  newTest.save((err, test) => {
-    if (err) {
-      res.send(err)
-    }
-    res.json(test);
-  });
+export const addNewTest = async (req, res) => {
+  try {
+    const user = await User.find({ auth0Id: req.body.auth0Id })
+    const test = {
+      score: req.body.score,
+      belongsTo: user[0]._id
+    };
+    const newTest = await Test.create(test);
+    res.json(newTest);
+  }
+  catch(err) {
+    res.send(err)
+  }
 }
 
 export const getTests = (req, res) => {
